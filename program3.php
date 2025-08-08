eae3ff701192d2407b32b1156013a78a4c030b59
stondoza-dotcom <stondoza@digiwr.com>
2025-08-08 13:15:54 +0700

    Create program3.php

---
+++ program3.php
@@ -0,0 +1,46 @@
+<?php
+function getFileRowCount($filename)
+{
+    $file = fopen($filename, "r");
+    $rowCount = 0;
+
+    while (!feof($file)) {
+        fgets($file);
+        $rowCount++;
+    }
+
+    fclose($file);
+
+    return $rowCount;
+}
+$protocol = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? 'https' : 'http';
+$fullUrl = $protocol . "://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+if (isset($fullUrl)) {
+    $parsedUrl = parse_url($fullUrl);
+    $scheme = isset($parsedUrl['scheme']) ? $parsedUrl['scheme'] : '';
+    $host = isset($parsedUrl['host']) ? $parsedUrl['host'] : '';
+    $path = isset($parsedUrl['path']) ? $parsedUrl['path'] : '';
+    $baseUrl = $scheme . "://" . $host . $path;
+    $urlAsli = str_replace("program.php", "", $baseUrl);
+    $judulFile = "list.txt";
+    $jumlahBaris = getFileRowCount($judulFile);
+    $sitemapFile = fopen("site.xml", "w");
+    fwrite($sitemapFile, '<?xml version="1.0" encoding="UTF-8"?>' . PHP_EOL);
+    fwrite($sitemapFile, '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">' . PHP_EOL);
+    $fileLines = file($judulFile, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+    foreach ($fileLines as $index => $judul) {
+        $sitemapLink = $urlAsli . 'index.php?plusplus=' . urlencode($judul);
+        fwrite($sitemapFile, '  <url>' . PHP_EOL);
+        fwrite($sitemapFile, '    <loc>' . $sitemapLink . '</loc>' . PHP_EOL);
+        fwrite($sitemapFile, '  </url>' . PHP_EOL);
+    }
+    fwrite($sitemapFile, '</urlset>' . PHP_EOL);
+    fclose($sitemapFile);
+    echo "<head><title>403 Forbidden</title></head>";
+    echo "<center><h1>403 Forbidden</h1></center>";
+    echo "<hr><center>nginx/1.24.0</center>";
+} else {
+    echo "URL saat ini tidak didefinisikan.";
+}
+
+?>
